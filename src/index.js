5d5c5263f6d015a8f6900ec2240a42c44dcd8c05
Ritchie Simmons <ritchiels.alt@gmail.com>
2024-02-22 22:21:30 -0500

    navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
 //file where we connect our app to the DOM
